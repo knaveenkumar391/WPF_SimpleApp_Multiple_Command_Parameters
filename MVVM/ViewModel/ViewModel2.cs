e4611e1fbdabf28c76fc5c5422b5f1d2c9ea85c0
knaveenkumar391 <knaveenkumar391@gmail.com>
2024-10-25 00:31:13 +0530

    2nd Commit with multiple viewModel in single view achived

---
+++ MVVM/ViewModel/ViewModel2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_SimpleApp_Multiple_Command_Parameters.MVVM.ViewModel
+{
+    public class ViewModel2
+    {
+        public int Counter { get; set; }
+        public ViewModel2()
+        {
+            Counter = 0;
+        }
+    }
+}
